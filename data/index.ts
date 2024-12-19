6713f11496877939b15d69bc08a799b437672bee
unknown <aliyunasirahmad70@gmail.com>
2024-12-19 11:28:03 +0100

    first commit

---
+++ data/index.ts
@@ -0,0 +1,238 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Resume", link: "https://drive.google.com/file/d/1wdy8GTnw4QEdrRnM6LKV3Z1mRrjbkJtM/view?usp=sharing" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/imgs.svg",
+    spareImg: "/smt.svg",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "A passionate and skilled Product Designer video edittor with over 6 years of Experience.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently Designing and Prototyping Edstream",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/edu.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to work with me!?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Edstream",
+    des: "A complete solution for administrators,teachers, parents, and students—simplifying management for primary, secondary, and tertiary institutions.",
+    img: "/edstreamBg.svg",
+    iconLists: ["/figma.svg", "/dribble.svg", "/behance.svg", "/framer.svg"],
+    link: "https://edstream.ng",
+    
+  },
+  {
+    id: 2,
+    title: "SabiKuk",
+    des: "Online cooking site offering quick, easy-to-follow recipes for a variety of global dishes. Perfect for all skill levels, it features step-by-step guides, meal planning tools, and dietary filters to make cooking enjoyable and accessible.",
+    img: "/sabiKuk.svg",
+    iconLists: ["/figma.svg", "/dribble.svg", "/behance.svg", "/framer.svg"],
+    link: "https://sabikuk.com/",
+  },
+  {
+    id: 3,
+    title: "Muaga",
+    des: "Online Tech company profile page that showcase what they offer to there customers.",
+    img: "/muaga.svg",
+    iconLists: ["/figma.svg", "/dribble.svg", "/behance.svg", "/framer.svg"],
+    link: "https://muaga.ng",
+  },
+  {
+    id: 4,
+    title: "Commusphere",
+    des: "Simplifying financial services with innovative solutions like secure digital payments, personal finance management, and seamless banking integration. It empowers users to take control of their finances with ease and efficiency.",
+    img: "/commusphere.svg",
+    iconLists: ["/figma.svg", "/dribble.svg", "/behance.svg", "/framer.svg"],
+    
+  },
+  {
+    id: 5,
+    title: "Kwadai Foods",
+    des: "Online cooking site offering quick, easy-to-follow vendor for a variety of global dishes. Perfect for all skill levels, it features step-by-step guides, meal planning tools, and dietary filters to make cooking enjoyable and accessible.",
+    img: "/kwadaiFoods.svg",
+    iconLists: ["/figma.svg", "/dribble.svg", "/behance.svg", "/framer.svg"],
+    link: "https://kwadaifoods.ng",
+  },
+  {
+    id: 6,
+    title: "SuntecNig",
+    des: "Online Tech company profile page that showcase what they offer to there customers.",
+    img: "/suntecNig.svg",
+    iconLists: ["/figma.svg", "/dribble.svg", "/behance.svg", "/framer.svg"],
+    link: "https://suntecnig.ng",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Working with Abdulateef Fawaz Faisu on our tech platform has been a game-changer. Their ability to transform complex ideas into user-friendly and innovative solutions exceeded our expectations. Their expertise and dedication made our product stand out in a competitive market.",
+    name: "Amina Abubakar",
+    title: " — Product Lead, MUAGA",
+  },
+  {
+    quote:
+      "Abdulateef Fawaz Faisu role as a Product designer played a pivotal role in refining and enhancing Edstream’s user interface, making our online management system more intuitive and effective. Their creative problem-solving and technical proficiency helped us better serve our clients with a seamless experience",
+    name: "Muhawiyya Kabir",
+    title: " — Operations Manager, Edstream",
+  },
+  {
+    quote:
+      "Abdulateef Fawaz Faisu brought fresh ideas and exceptional design to our food delivery app, streamlining operations and improving customer engagement. Their innovative solutions helped us deliver meals faster and enhanced user satisfaction.",
+    name: "Umar Musa Gafia",
+    title: " — CEO, SabiKuk",
+  },
+   {
+    quote:
+      "Suntecnig's partnership with Abdulateef Fawaz Faisu as a product Designer resulted in cutting-edge features for our tech platform, improving user retention and scalability. Their ability to align technology with business goals is truly commendable",
+    name: "Usman Ishaq Karofi",
+    title: " — CEO, Suntecnig",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "Edstream",
+    // img: "/edstreamLogo.svg",
+    nameImg: "/edstreamLogo.svg",
+  },
+  {
+    id: 2,
+    name: "Boxify",
+    // img: "/app.svg",
+    nameImg: "/BoxifyLight.svg",
+  },
+  {
+    id: 3,
+    name: "Muaga",
+    //img: "/host.svg",
+    nameImg: "/muagaWhite.svg",
+  },
+  {
+    id: 4,
+    name: "SuntecNig",
+   // img: "/s.svg",
+    nameImg: "/SuntechLogo.svg",
+  },
+  {
+    id: 5,
+    name: "Commusphere.",
+    //img: "/dock.svg",
+    nameImg: "/commusphereLogo.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Product Designer",
+    desc: "I create intuitive and User- Centered designs, focusing on seamless user experiences through tools like Figma and Adobe XD. My goal is to bridge creativity and functionality to solve real-world problems.",
+    className: "md:col-span-2",
+    thumbnail: "/des.svg",
+  },
+  {
+    id: 2,
+    title: "Graphics || Brand Designer",
+    desc: "I craft impactful visual identities and compelling designs that help brands stabd out. With expertise in tools like Photoshop, illustrator and CorelDraw, I bring ideas to life with percision and creativity.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/gd.svg",
+  },
+  {
+    id: 3,
+    title: "Video Edittor",
+    desc: "I produce engaging and polished video content that tells a story and connects with audiences. Using tools like Wondershare Filmora and Capcut, I transform raw footage into captivating visuals. ",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/vd.svg",
+  },
+  {
+    id: 4,
+    title: "3D Animator",
+    desc: "I bring ideas to life through dynamic and realistic 3D animations. Leveraging tools like Creative Studio,I create immersive visuals that add depth and creativity to any project .",
+    className: "md:col-span-2",
+    thumbnail: "/3d.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Esbebe", // GitHub link
+  },
+  {
+    id: 2,
+    img: "/figma.svg",
+    link: "https://www.figma.com/@abdulateeffawaz", // Twitter link
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://ng.linkedin.com/in/abdulateef-fawaz-faisu-52549620b", // LinkedIn link
+  },
+  {
+    id: 4,
+    img: "/behance.svg",
+    link: "https://www.behance.net/abdulateeffawaz", // Behance link
+  },
+];
+
